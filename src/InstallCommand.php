69a013e37d401b7453959d1923f8b9a099fa9df9
Mike Barlow <mike@mikebarlow.co.uk>
2018-09-08 16:17:33 +0100

    Added question for install command

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -3,9 +3,9 @@
 namespace Helpflow\Installer;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class CreateUserCommand extends Command
 {
@@ -13,13 +13,20 @@ class CreateUserCommand extends Command
     {
         $this
             ->setName('helpflow:install')
-            ->setDescription('Install a new version of Helpflow')
-            ->addArgument('type', InputArgument::REQUIRED, 'The type of install; "laravel", "spark"');
+            ->setDescription('Install a new version of Helpflow');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $installType = $input->getArgument('type');
+        $helper = $this->getHelper('question');
+        $question = new ChoiceQuestion(
+            'Please select your system (defaults to Laravel Spark)',
+            ['Laravel Spark'],
+            0
+        );
+
+        $type = $helper->ask($input, $output, $question);
+
 
 
     }
