22e22bc028b1e48d3b9525cc67ac70fd36d31c01
Mike Barlow <mike@mikebarlow.co.uk>
2018-09-10 23:47:46 +0100

    First version of the installer working

--- src/InstallCommand.php
+++ src/InstallCommand.php
@@ -4,6 +4,7 @@ namespace Helpflow\Installer;
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,6 +13,10 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class InstallCommand extends Command
 {
+    protected $path;
+    protected $type;
+    protected $progressBar;
+
     protected function configure()
     {
         $this
@@ -28,20 +33,64 @@ class InstallCommand extends Command
             ['Laravel Spark'],
             0
         );
-        $type = $helper->ask($input, $output, $question);
-        $path = rtrim($input->getArgument('path'), '/');
+        $this->type = $helper->ask($input, $output, $question);
+        $this->path = rtrim($input->getArgument('path'), '/');
+        $this->progressBar = new ProgressBar($output, 6);
 
-        // git checkout helpflow
-        $process = new Process('git checkout git@github.com:mikebarlow/helpflow.git ' . $path . '/helpflow');
+        // git clone helpflow
+        $this->cloneRepo($output);
+
+        // add to composer file
+        $this->setupComposerFile($output);
+
+        // composer install
+        $this->composerInstall($output);
+
+        // add service provider
+        $this->addServiceProviders($output);
+
+        // migrate
+        $this->runMigrations($output);
+
+        // publish helpflow tag
+        $this->vendorPublish($output);
+
+        $completeMsg = [
+            '<info>Helpflow installation completed successfully!</info>',
+        ];
+
+        if ($this->type === 'Laravel Spark') {
+            $completeMsg[] = '<info>As you are using Laravel Spark, don\'t forget to add the links within the Kiosk to Helpflow. See the readme for more information.</info>';
+        }
+
+        $output->writeln($completeMsg);
+    }
+
+    public function cloneRepo($output)
+    {
+        $output->writeln([
+            '<comment>Starting Git clone</comment>',
+            '<comment>==================</comment>'
+        ]);
+        $process = new Process('git clone git@github.com:mikebarlow/helpflow.git ' . $this->path . '/helpflow');
         $process
             ->setTimeout(null)
             ->run(function ($type, $line) use ($output) {
                 $output->write($line);
             });
 
-        // add to composer file
+        $this->progressBar->advance();
+        $output->writeln('');
+    }
+
+    public function setupComposerFile($output)
+    {
+        $output->writeln([
+            '<comment>Starting Composer.json updates</comment>',
+            '<comment>==============================</comment>'
+        ]);
         $composerJson = json_decode(
-            file_get_contents($path . '/composer.json'),
+            file_get_contents($this->path . '/composer.json'),
             true
         );
         if (! empty($composerJson['repositories'])) {
@@ -60,15 +109,44 @@ class InstallCommand extends Command
         $composerJson['require']['helpflow/helpflow'] = '*@dev';
 
         // composer require admin type
-        if ($type === 'Laravel Spark') {
+        if ($this->type === 'Laravel Spark') {
             $composerJson['require']['helpflow/spark-admin'] = '*@dev';
         }
 
         // save composer
-        file_put_contents($path . '/composer.json', $composerJson);
+        file_put_contents(
+            $this->path . '/composer.json',
+            json_encode($composerJson, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)
+        );
 
-        // add service provider
-        $appConfig = file_get_contents($path . '/config/app.php');
+        $this->progressBar->advance();
+        $output->writeln('');
+    }
+
+    public function composerInstall($output)
+    {
+        $output->writeln([
+            '<comment>Running composer update</comment>',
+            '<comment>=======================</comment>'
+        ]);
+        $process = new Process('cd ' . $this->path . ' && composer update');
+        $process
+            ->setTimeout(null)
+            ->run(function ($type, $line) use ($output) {
+                $output->write($line);
+            });
+
+        $this->progressBar->advance();
+        $output->writeln('');
+    }
+
+    public function addServiceProviders($output)
+    {
+        $output->writeln([
+            '<comment>Add Helpflow Service Providers</comment>',
+            '<comment>==============================</comment>'
+        ]);
+        $appConfig = file_get_contents($this->path . '/config/app.php');
 
         $appConfig = str_replace(
             '        App\\Providers\\AppServiceProvider::class,',
@@ -76,7 +154,7 @@ class InstallCommand extends Command
             $appConfig
         );
 
-        if ($type === 'Laravel Spark') {
+        if ($this->type === 'Laravel Spark') {
             $adminProvider = 'Helpflow\SparkAdmin\Providers\HelpflowSparkAdminServiceProvider::class';
         }
 
@@ -86,26 +164,46 @@ class InstallCommand extends Command
             $appConfig
         );
 
-        file_put_contents($path . '/config/app.php', $appConfig);
+        file_put_contents(
+            $this->path . '/config/app.php',
+            $appConfig
+        );
 
-        // migrate
-        $process = new Process('php ' . $path . '/artisan migrate');
+        $this->progressBar->advance();
+        $output->writeln('');
+    }
+
+    public function runMigrations($output)
+    {
+        $output->writeln([
+            '<comment>Starting DB Migrate</comment>',
+            '<comment>===================</comment>'
+        ]);
+        $process = new Process('php ' . $this->path . '/artisan migrate');
         $process
             ->setTimeout(null)
             ->run(function ($type, $line) use ($output) {
                 $output->write($line);
             });
 
-        // publish helpflow tag
-        $process = new Process('php ' . $path . '/artisan vendor:publish --tag=helpflow');
+        $this->progressBar->advance();
+        $output->writeln('');
+    }
+
+    public function vendorPublish($output)
+    {
+        $output->writeln([
+            '<comment>Starting asset publishing</comment>',
+            '<comment>=========================</comment>'
+        ]);
+        $process = new Process('php ' . $this->path . '/artisan vendor:publish --tag=helpflow');
         $process
             ->setTimeout(null)
             ->run(function ($type, $line) use ($output) {
                 $output->write($line);
             });
 
-
-        $output->writeln('Finished');
-        // if type is laravel spark, message to add admins to the developers section for kiosk access
+        $this->progressBar->advance();
+        $output->writeln('');
     }
 }
